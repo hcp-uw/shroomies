c6e712a438ce7253047602443ce000bf2bdccb6f
pteran <pteran@uw.edu>
2024-02-13 10:40:39 -0800

    SafeAreaView and got camera functioning

--- client/src/HomeScreenMVP.js
+++ client/src/HomeScreenMVP.js
@@ -1,49 +1,60 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import * as ImagePicker from 'expo-image-picker';
 
 const HomeScreenMVP = () => {
   const [searchText, setSearchText] = useState('');
+  const [image, setImage] = useState(null);
 
-  const launchCamera = () => {
-    let options = {
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-    launchCamera(options, response => {
-      console.log('Response = ', response);
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else {
-        console.log('response', JSON.stringify(response));
-        this.setState({
-          fileUri: response.assets[0].uri,
-        });
+  const uploadImage = async () => {
+    try {
+      await ImagePicker.requestCameraPermissionsAsync();
+      let result = await ImagePicker.launchCameraAsync({
+        cameraType: ImagePicker.CameraType.back,
+        allowsEditing: true,
+        spect: [1, 1],
+        quality: 1
+      });
+
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
       }
-    });
-  };
+    } catch (error) {
+      alert("Error uploading image");
+    }
+  }
 
   return (
-    <LinearGradient
-      colors={['#EE9E62', '#576F3D']}
-      style={styles.gradient}
-    >
-      <View style={styles.container}>
-        <Text style={styles.title}>Fungi Find</Text>
-        <Text style={styles.subtitle}>Scan your mushroom to identify!</Text>
-        <View style={styles.horizIconContainer}>
-          <Image
-            source={require('../assets/camera-fungifind 1.png')}
-            style={styles.rotatedIcon15}
-          />
-          <Image
-            source={require('../assets/shroomies vector icon 1.png')}
-            style={styles.icon}
-          />
-        </View>
+    <SafeAreaView style={{flex: 1}}>
+      <LinearGradient
+        colors={['#EE9E62', '#576F3D']}
+        style={styles.gradient}
+      >
+        <View style={styles.container}>
+          <Text style={styles.title}>Fungi Find</Text>
+          <Text style={styles.subtitle}>Scan your mushroom to identify!</Text>
+          <View style={styles.horizIconContainer}>
+            <Image
+              source={require('../assets/camera-fungifind 1.png')}
+              style={styles.rotatedIcon15}
+            />
+            <Image
+              source={require('../assets/shroomies vector icon 1.png')}
+              style={styles.icon}
+            />
+          </View>
+          <View style={styles.searchBarContainer}>
+            <TextInput
+              style={styles.searchBar}
+              placeholder="Search..."
+              value={searchText}
+              onChangeText={(text) => setSearchText(text)}
+              onFocus={() => setSearchText('')}
+            />
+            <Text style={styles.searchText}>Search</Text>
+          </View>
+
         <View style={styles.searchBarContainer}>
           <TextInput
             style={styles.searchBar}
@@ -54,27 +65,17 @@ const HomeScreenMVP = () => {
           />
           <Text style={styles.searchText}>Search</Text>
         </View>
-        
-      <View style={styles.searchBarContainer}>
-        <TextInput
-          style={styles.searchBar}
-          placeholder="Search..."
-          value={searchText}
-          onChangeText={(text) => setSearchText(text)}
-          onFocus={() => setSearchText('')}
-        />
-        <Text style={styles.searchText}>Search</Text>
-      </View>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={this.launchCamera}
-          style={styles.button}>
-          <Text style={styles.buttonText}>Scan Mushroom</Text>
-        </TouchableOpacity>
-      </View>
-      </View>
-    </LinearGradient>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={uploadImage}
+            style={styles.button}>
+            <Text style={styles.buttonText}>Scan Mushroom</Text>
+          </TouchableOpacity>
+        </View>
+        </View>
+      </LinearGradient>
+    </SafeAreaView>
   );
 }
 
