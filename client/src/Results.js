b1ee96701dcdaeb6d69c0fdaab741bdbc3dddc51
pteran <pteran@uw.edu>
2024-04-05 12:53:45 -0700

    results page progress

--- client/src/Results.js
+++ client/src/Results.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, Image, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
 
 const Results = ({navigation}) => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF2E9' }}>
       <View style={styles.headingContainer}>
+        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+          <Image
+            source={require('../assets/arrow-fungifind.png')}
+            style={styles.arrow}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.imageContainer}>
         <Image
-          source={require('../assets/arrow-fungifind.png')}
-          style={styles.arrow}
+          style={styles.image}
         />
+      </View>
+      <View style={styles.infoContainer}>
+        <Image
+          source={require('../assets/shroomies-x.png')}
+          style={styles.resultsImage}
+        />
+        <Text style={styles.resultsText}>Poisonous</Text>
+        <Text style={styles.disclaimer}>
+          Disclaimer: This app is developed by students for educational and recreational purposes.
+          Users must assume responsibility for actions taken related to mushroom identification, such
+          as consumption and other associated risks.
+        </Text>
       </View>
     </SafeAreaView>
   );
@@ -16,10 +35,48 @@ const Results = ({navigation}) => {
 
 const styles = StyleSheet.create({
   headingContainer: {
-    flex: 1,
+    padding: 16
   },
   arrow: {
     transform: [{ rotate: '180deg'}],
+    width: 40,
+    height: 40,
+  },
+  imageContainer: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 0.5,
+    flex: 0.6,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 10,
+    backgroundColor: 'pink',
+    marginBottom: 10
+  },
+  infoContainer: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 16,
+    paddingHorizontal: 30
+  },
+  resultsImage: {
+    width: 125,
+    height: 125
+  },
+  resultsText: {
+    fontWeight: 'bold',
+    fontSize: 33,
+    lineHeight: 40,
+    color: '#772F1A',
+  },
+  disclaimer: {
+    fontSize: 16,
+    lineHeight: 20,
+    color: '#772F1A',
+    marginTop: 15
   }
 })
 
