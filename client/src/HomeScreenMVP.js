c166b02f6d0092399cc6d8b651fc7a7031c405da
pteran <pteran@uw.edu>
2024-09-01 13:49:27 -0700

    small comment

--- client/src/HomeScreenMVP.js
+++ client/src/HomeScreenMVP.js
@@ -15,7 +15,7 @@ const HomeScreenMVP = ({ navigation }) => {
 
   publicIP()
   .then(ip => {
-    // ip = "127.0.0.1";
+    // ip = "127.0.0.1"; works with this ip for ios simulator
     ip = "192.168.0.183"; //directly pasted in from server startup output (second ip listed)
     serverURL = "http://" + ip + ":4000/identify";
     console.log(serverURL);
